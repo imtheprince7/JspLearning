7b0d9987a2c8431c078c609ef2bba1a081b85a4e
Prince <princejnu7@hotmail.com>
2023-02-12 17:34:41 +0530

    jsp

---
+++ src/java/JspNotes/JspTags1.java
@@ -0,0 +1,36 @@
+package JspNotes;
+public class JspTags1 {
+/*
+        What is Jsp
+       Jsp is used to create web application just like Servlet technology.
+       Similar to Servlet( functionality)
+       It's enhance version of servlet
+       IN JSP we write java code in HTML pages ( Servlet:- We write Html CODE IN JAVA LANG.
+    SERVLET:--- WAS HARD TO WRITE BECAUSE WE CAN'T WRITE WHOLE HTML PAGE IN JAVA CODE.
+     ORACLE DEVELOP JSP to make easy write and easy web Development.
+    
+  -----------------------  JSP TAGS ------------- To write Java code inside HTML PAGE
+    1.  Declarative Tag
+    2. Scriptlet Tag
+    3. Expression Tag
+            Declarative Tag:
+                Syntax:  <%!    %>
+                Inside we can declare variable perform opeartion( Bussines Logic) Methods() )
+    
+            Scriptlet Tag:
+                Syntax:   <%     %>
+                Inside we can write content of java
+                We can write processing work
+                
+            Expression Tag:
+                Synatx:  <%=   %>
+                    It's use to print same as we write in page.
+             
+    
+    
+    
+    
+    
+    
+ */    
+}
