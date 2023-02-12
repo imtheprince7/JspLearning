3443c33fad07372885cab29b31d863ec3fe48339
Prince <princejnu7@hotmail.com>
2023-02-12 17:51:49 +0530

    jspnotes

--- src/java/JspNotes/JspPageDirective.java
+++ src/java/JspNotes/JspPageDirective.java
@@ -12,6 +12,20 @@ public class JspPageDirective {
     1. Page Directives: (  <%@page    %>   )
                 We can use PAGE DIRECTIVES tags insider web-page at any place.
     
+     
+    2). Include Directives: ( <%@include %>  )
+        It's use to include other resources in pages. ( Eg: Navbar, Footer, forms etc)
+    EG:--- Navbar Footer make in different place and includes in all pages
+            less Code, multiple use in project.
+
     
-*/    
+    
+    3). Taglib Directives: 
+            It's use when we want to use other tag library in our JSP pages.
+           EG:  JSTL ( Jsp Standard Tag Libraries ) or custom library created by users.
+
+
+
+
+*/ 
 }
